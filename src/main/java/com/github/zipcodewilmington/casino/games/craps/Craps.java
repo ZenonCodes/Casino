8083a3ee49e3784c8d31355b65e1790c46fa2a48
Miles Coney <MilesEConey@gmail.com>
2022-03-18 10:23:26 -0400

    Resolved bracket issue

--- src/main/java/com/github/zipcodewilmington/casino/games/craps/Craps.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/craps/Craps.java
@@ -67,9 +67,9 @@ public class Craps {
     private static boolean getRules(Scanner scanner) {
         System.out.println("\nWould you like to learn how to play Craps? (Yes/No)");
         String replay = scanner.nextLine();
-        if (replay.equals("Yes")){
+        if (replay.equals("Yes")) {
             return gameObjectiveMessage();
-        } else if (replay.equals("No")){
+        } else if (replay.equals("No")) {
             return false;
         }
         return false;
@@ -100,15 +100,15 @@ public class Craps {
         System.out.println("\nWould you like to play Craps? (Yes/No)");
         String replay = scanner.nextLine();
 
-        if (replay.equals("Yes")){
+        if (replay.equals("Yes")) {
             return true;
-        } else if (replay.equals("No")){
+        } else if (replay.equals("No")) {
             return false;
         }
         return false;
     }
 
-
+}
 
 
 
