bbcb8b325826f8cc518663622d98669949920ff2
Miles Coney <MilesEConey@gmail.com>
2022-03-21 00:53:14 -0400

    Updated Craps and Slots to have the waging ability, and quit out to main menu, also added colors from console. Could not get testing :(

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,10 +1,13 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
+import com.github.zipcodewilmington.Casino;
 import com.github.zipcodewilmington.casino.CasinoAccount;
 import com.github.zipcodewilmington.casino.GameInterface;
 import com.github.zipcodewilmington.casino.WagingGame;
 import com.github.zipcodewilmington.casino.WagingPlayer;
 import com.github.zipcodewilmington.casino.cards.Cards;
+import com.github.zipcodewilmington.utils.AnsiColor;
+import com.github.zipcodewilmington.utils.IOConsole;
 
 import java.util.ArrayDeque;
 import java.util.List;
@@ -15,6 +18,12 @@ import java.util.Scanner;
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame extends WagingGame implements GameInterface<SlotsPlayer> {
+    final IOConsole console = new IOConsole(AnsiColor.AUTO);
+    SlotsPlayer player;
+    int playerBet;
+    int winnings;
+    int newPlayerBalance;
+
 
     @Override
     public Boolean isOver() {
@@ -23,7 +32,7 @@ public class SlotsGame extends WagingGame implements GameInterface<SlotsPlayer> 
 
     @Override
     public void addPlayer(SlotsPlayer player) {
-
+        this.player = player;
     }
 
     @Override
@@ -48,36 +57,74 @@ public class SlotsGame extends WagingGame implements GameInterface<SlotsPlayer> 
 
     @Override
     public void run() {
+
         Random generator = new Random();
-//        Scanner console = new Scanner(System.in);
-
-        int input;
-        int wager = 0;
+        int playerBalance = player.getAccountBalance();
         int slot1, slot2, slot3;
-        do {
-            System.out.println("Slot Machine");
-            System.out.println("How many tokens would you like to wager");
-            wager = getUserInput();
-            System.out.println("Press 1 to pull, 2 to quit");
 
-            input = getUserInput();
+        welcomeMessage();
+
+        console.println("\n\nYou have " + player.getAccountBalance() + " to bet.\n");
+
+        do {
+
+            player.setBet();
+            playerBet = player.getBet();
+
+            console.println("\nPress 1 to pull, 2 to quit");
+            getUserInput();
 
             slot1 = generator.nextInt(6)+1; //the bound number is setting the 3 range of numbers, the number after the + is where the number starts so ex) + 7 would give combos of 789
             slot2 = generator.nextInt(6)+1;
             slot3 = generator.nextInt(6)+1;
 
-            System.out.println(slot1 + " " + slot2 + " " + slot3);
+            slotWheel(slot1, slot2, slot3);
 
             if (slot1 == slot2 && slot1 == slot3) {
-                System.out.println("You win 10 tokens");
-                wager += 10;
+                winnings = (playerBet * 2);
+                youWinMessage();
+                newPlayerBalance = playerBalance + winnings;
+                player.getCasinoAccount().setAccountBalance(newPlayerBalance);
+
             } else if (slot1 == slot2 || slot2 == slot3) {
-                System.out.println("You lost 5 tokens");
-                wager += 5;
+                winnings = (int) (playerBet * 1.25);
+                youWinMessage();
+                newPlayerBalance = playerBalance + winnings;
+                player.getCasinoAccount().setAccountBalance(newPlayerBalance);
+
             } else {
-                System.out.println("You lost a token");
-                wager -= 1;
+                youLoseMessage();
+                newPlayerBalance = playerBalance - playerBet;
+                player.getCasinoAccount().setAccountBalance(newPlayerBalance);
+
             }
-        } while (input == 1);
+
+            console.println("\nPlay again? Press 1, Press 2 to quit");
+
+        } while (getUserInput() == 1);
+    }
+
+    private void youLoseMessage() {
+        final IOConsole console = new IOConsole(AnsiColor.RED);
+        console.println("You lost " + playerBet);
+    }
+
+    private void youWinMessage() {
+        final IOConsole console = new IOConsole(AnsiColor.YELLOW);
+        console.println("You win " + winnings + "\n");
+    }
+
+    private void slotWheel(int slot1, int slot2, int slot3) {
+        final IOConsole console = new IOConsole(AnsiColor.PURPLE);
+        console.println("\n" + slot1 + "   " + slot2 + "   " + slot3 + "\n");
+    }
+
+    private void welcomeMessage() {
+        final IOConsole console = new IOConsole(AnsiColor.CYAN);
+        console.println("\nWelcome to the Slot Machine!!!" +
+                "\n\nPull the lever to try your luck," +
+                "\n3 in a row and you double your bet!!" +
+                "\n2 matching and you get a little for your effort," +
+                "\nif you have no matching reels you LOSE!\n");
     }
 }
